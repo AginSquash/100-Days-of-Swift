3f016580e83379135a33e38cb42a190a58ef78d1
AginS <agins.main@gmail.com>
2020-07-09 01:28:46 +0300

    day 16 fin

---
+++ Days 16 - 18 Project 1/Project1/Project1/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Vlad Vrublevsky on 09.07.2020.
+//  Copyright © 2020 Vlad Vrublevsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        print(pictures)
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+}
+
