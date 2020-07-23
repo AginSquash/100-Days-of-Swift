9dd123c9504572ce59ef343b92e6da21dec804ff
AginS <agins.main@gmail.com>
2020-07-23 19:29:14 +0300

    day 32 complete

---
+++ Days 32 - 32 Challenge/Challenge32/Challenge32/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Challenge32
+//
+//  Created by Vlad Vrublevsky on 23.07.2020.
+//  Copyright © 2020 Vlad Vrublevsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var list = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showAddAlert))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(refresh))
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         return list.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "element_cell", for: indexPath)
+        cell.textLabel?.text = list[indexPath.row]
+        return cell
+    }
+    
+    @objc func showAddAlert() {
+        let ac = UIAlertController(title: "Enter new element", message: "", preferredStyle: .alert)
+        ac.addTextField()
+        
+        let addElementAction = UIAlertAction(title: "Add", style: .default) { [weak self, weak ac] action in
+            guard let element = ac?.textFields?[0].text else { return }
+            self?.addElement(element)
+        }
+        ac.addAction(addElementAction)
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
+ 
+    @objc func refresh() {
+        list = [String]()
+        tableView.reloadData()
+    }
+    
+    func addElement(_ element: String) {
+        list.insert(element, at: 0)
+        let indexpath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexpath], with: .automatic)
+    }
+}
+
