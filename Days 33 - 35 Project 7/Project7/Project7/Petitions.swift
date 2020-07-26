40e2867c56dac6d02e666c3f93c4a6e87217de57
AginS <agins.main@gmail.com>
2020-07-26 20:26:25 +0300

    Day 34 complete

---
+++ Days 33 - 35 Project 7/Project7/Project7/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Vlad Vrublevsky on 26.07.2020.
+//  Copyright © 2020 Vlad Vrublevsky. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
