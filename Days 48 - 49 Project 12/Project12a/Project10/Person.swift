e96e6f9827a158b0013db69dc89e0618d4aa737a
AginS <agins.main@gmail.com>
2020-08-23 02:50:26 +0300

    Day 49 completed

---
+++ Days 48 - 49 Project 12/Project12a/Project10/Person.swift
@@ -0,0 +1,30 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Vlad Vrublevsky on 21.08.2020.
+//
+
+import UIKit
+
+class Person: NSObject, NSCoding {
+    
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+    
+    func encode(with coder: NSCoder) {
+        coder.encode(name, forKey: "name")
+        coder.encode(image, forKey: "image")
+    }
+    
+    required init?(coder: NSCoder) {
+        self.name = coder.decodeObject(forKey: "name") as? String ?? ""
+        self.image = coder.decodeObject(forKey: "image") as? String ?? ""
+    }
+    
+}
