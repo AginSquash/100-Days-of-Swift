258f6bced00a019b39a915dbd8bd7043834180bd
AginS <agins.main@gmail.com>
2020-10-02 14:29:51 +0300

    2/3 challenges completed

---
+++ Days 60 - 61 Project 16/Project16/Project16/Capital.swift
@@ -0,0 +1,21 @@
+//
+//  Capital.swift
+//  Project16
+//
+//  Created by Vlad Vrublevsky on 02.10.2020.
+//
+
+import UIKit
+import MapKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
