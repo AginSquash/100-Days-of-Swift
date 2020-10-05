c7d638fab66c038bd3574529db4d5e1205309f93
AginS <agins.main@gmail.com>
2020-10-05 13:36:57 +0300

    days 64-66 completed

---
+++ Days 64 - 65 Project 18/Project18/Project18/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by Vlad Vrublevsky on 04.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        for i in 1 ... 100 {
+            print("Got number \(i)")
+        }
+    }
+
+
+}
+
