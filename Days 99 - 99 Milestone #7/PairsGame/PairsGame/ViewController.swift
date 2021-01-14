f0db7c9131330dfa5fea1b2869c62074f96e69f3
AginS <agins.main@gmail.com>
2021-01-14 22:40:57 +0300

    added alg for random check correct

--- Days 99 - 99 Milestone #7/PairsGame/PairsGame/ViewController.swift
+++ Days 99 - 99 Milestone #7/PairsGame/PairsGame/ViewController.swift
@@ -11,6 +11,10 @@ class ViewController: UICollectionViewController {
     var pairs: [Pair] = Pair.getExample()
     var used_pairs: [Pair] = []
     
+    
+    var pairschain: [String] = []
+    var currentlySelectedItem: Int?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -19,7 +23,9 @@ class ViewController: UICollectionViewController {
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return pairs.count * 2
+        let count = pairs.count * 2
+        pairschain = repeatElement("0", count: count).map( { String($0) })
+        return count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -32,8 +38,10 @@ class ViewController: UICollectionViewController {
         
         if pair.status == 0 {
             cell.label.text = pair.capital
+            pairschain[indexPath.item] = pair.country
         } else {
             cell.label.text = pair.country
+            pairschain[indexPath.item] = pair.capital
         }
         pair.status += 1
         
@@ -42,6 +50,27 @@ class ViewController: UICollectionViewController {
         }
         
         return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("user selected: \(indexPath.item)")
+        let index = indexPath.item
+        
+        if currentlySelectedItem == nil {
+            currentlySelectedItem = index
+            print(pairschain[index])
+            // animate here
+            return
+        }
+        
+        let cell = collectionView.cellForItem(at: indexPath) as! CardCellView
+        if cell.label.text == pairschain[currentlySelectedItem!] {
+            print("You're right!")
+        } else {
+            print("No!")
+        }
+        print(pairschain[index])
+        currentlySelectedItem = nil
     }
 
 }
